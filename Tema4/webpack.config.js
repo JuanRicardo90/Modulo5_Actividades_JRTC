0f53ee7bced67c17df06e7912d407aa12443cb12
JuanRicardo90 <jrtuckar_654@hotmail.com>
2021-11-27 21:02:50 -0600

    Actividades de la 2-6

---
+++ Tema4/webpack.config.js
@@ -0,0 +1,28 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname,'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.?js$/,
+                use: {
+                    loader: "babel-loader",
+                    options:{
+                    presets: ['@babel/preset-env', '@babel/preset-react'],
+                    exclude: /node-modules/,
+                    }
+                 }
+            }
+        ]
+    },
+    plugins: [
+            new HtmlWebpackPlugin({
+                template: path.join(__dirname, "src", "index.html"),
+            })
+    ]
+}
